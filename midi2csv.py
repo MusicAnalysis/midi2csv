6a49633b3d3e6fca7800b546663636ca18b3c3ed
Lewis Lin <lewislin@yahoo.com>
2020-07-01 17:47:35 -0400

    Sort by onsettime

--- midi2csv.py
+++ midi2csv.py
@@ -2,18 +2,34 @@ import csv
 import os
 import pretty_midi
 import sys
+from operator import itemgetter, attrgetter
+
+class Note:
+	def __init__(self,p,ont,offt,d,vel):
+		self.pitch=p
+		self.start=float(ont)
+		self.end=float(offt)
+		self.duration=float(d)
+		self.velocity=int(vel)
+		
+	def __str__(self):
+		return str(self.__dict__)
 
 def process_midi(midi_file):
     print("processing " +  midi_file)
     csv_file = midi_file.replace(".mid", ".csv")
     # Load MIDI file into PrettyMIDI object
     midi_data = pretty_midi.PrettyMIDI(midi_file)
+    midi_list = []
     with open(csv_file, mode='w') as output_file:
         csv_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(["pitch", "start", "end", "duration", "velocity"])
         for instrument in midi_data.instruments:
             for note in instrument.notes:
-                csv_writer.writerow([pretty_midi.note_number_to_name(note.pitch), "{:0>3.4f}".format(note.start), "{:0>3.4f}".format(note.end), "{:3.4f}".format(note.duration), note.velocity])
+                midi_list.append(Note(note.pitch,note.start,note.end,note.duration,note.velocity))
+        midi_list.sort(key=attrgetter('start'))
+        for note in midi_list:
+            csv_writer.writerow([pretty_midi.note_number_to_name(note.pitch), "{:0>3.4f}".format(note.start), "{:0>3.4f}".format(note.end), "{:3.4f}".format(note.duration), note.velocity])
 
 
 
